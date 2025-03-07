e69910c216acb40b86b4bdbeee19e8b5bcb22dea
vicenttcarvalho <vicentecleyton@gmail.com>
2025-03-07 20:00:19 -0300

    feat: Adding the features of the ContaTerminal class

--- contabanco/src/main/java/com/vicenttcarvalho/ContaTerminal.java
+++ contabanco/src/main/java/com/vicenttcarvalho/ContaTerminal.java
@@ -1,7 +1,28 @@
 package com.vicenttcarvalho;
 
+import java.util.Locale;
+import  java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Por favor, digite o núemero da Agência: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite a Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("\nOlá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo R$"+saldo+" já está disponível para saque.");
+
+
+        scanner.close();
     }
 }
